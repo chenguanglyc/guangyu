2b09bc6b732f5164410cbe9ce6804a9a269446b8
lyc <920442875@qq.com>
2017-05-04 20:26:45 +0800

    用例2

--- guangyu/src/main/maxLength.java
+++ guangyu/src/main/maxLength.java
@@ -33,7 +33,7 @@ public class maxLength {
         return maxLength;  
     }  
     public static void main(String[] args) {  
-        String str="abcdaf";  
+        String str="aaaaaaa";  
         System.out.println(maxLength(str));  
   
     }  
